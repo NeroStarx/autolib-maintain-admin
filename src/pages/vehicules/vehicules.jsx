edb03b82837d5634b02553fcc9b0a5a8cbc38a29
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-05-26 02:57:30 +0100

    finish: vehicule list view

--- src/pages/vehicules/vehicules.jsx
+++ src/pages/vehicules/vehicules.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import VehiculeView from '../../components/vehiculeview/vehiculeListView'
+import './vehicules.css'
 
 
 export default function vehicules(props){
     return(
-        <h1>hi vehicules</h1>
+        <div className="vehicule-list-container">
+            <div className="vehicule-header-container">
+                <p className="vehicule-header-title">Véhicule</p>
+                <p className="vehicule-header-title">Client</p>
+                <p className="vehicule-header-title">Disponible le</p>
+                <p className="vehicule-header-title">Status</p>
+            </div>
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+            <VehiculeView />
+        </div>
     )
 }
