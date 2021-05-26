5d795355eef698839a5d952c01060397d0292029
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-05-26 23:43:17 +0100

    fix: agents layout to be better

--- src/components/agentview/agentview.jsx
+++ src/components/agentview/agentview.jsx
@@ -5,12 +5,14 @@ import image from '../../assets/profile_pic.png'
 export default function AgentView(props){
     return(
         <div className="agent-view-container">
-            <img src={image} alt="agent" className="agent-image"/>
-            <div className="agent-divider-info">
-                <p className="agent-title">Agent name</p>
-                <p className="agent-subtitle">updated 10mins ago</p>
+            <div className="agent-info">
+                <img src={image} alt="agent" className="agent-image"/>
+                <div className="agent-divider">
+                    <p className="agent-title">Agent name</p>
+                    <p className="agent-subtitle">updated 10mins ago</p>
+                </div>
             </div>
-            <div className="agent-divider-task">
+            <div className="agent-divider">
                 <p className="agent-title">Netoyer véhicule N200</p>
                 <p className="agent-subtitle">20 mars 2021</p>
             </div>
